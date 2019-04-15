d51b22fb8efa584d76c36ee1b997e2ae3a0f9d01
Carla Bosco <carlabosco@gmail.com>
2019-04-15 16:26:21 -0700

    Added drivers_controller

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -1,2 +1,54 @@
 class DriversController < ApplicationController
+  def index
+    @drivers = Driver.all
+  end
+
+  def show
+    driver_id = params[:id]
+    @driver = Driver.find_by(id: driver_id)
+
+    unless @driver
+      head :not_found
+    end
+  end
+
+  def edit
+    driver_id = params[:id]
+    @driver = Driver.find_by(id: driver_id)
+
+    unless @driver
+      head :not_found
+    end
+  end
+
+  def update
+    driver = Driver.find_by(id: params[:id])
+
+    unless driver
+      head :not_found
+      return
+    end
+
+    driver.update(driver_params)
+    redirect_to driver_path(driver)
+  end
+
+  def destroy
+    driver_id = params[:id]
+    driver = Driver.find_by(id: driver_id)
+
+    unless driver
+      head :not_found
+      return
+    end
+
+    driver.destroy
+    redirect_to drivers_path
+  end
+
+  private
+
+  def driver_params
+    return params.require(:driver).permit(:name, :vin)
+  end
 end
