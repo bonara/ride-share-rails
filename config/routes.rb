2508c8c575c7affafa5a757628ea4bed45ad86a1
Carla Bosco <carlabosco@gmail.com>
2019-04-17 13:34:46 -0700

    Updated drivers table

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   root 'homepages#index'
-  resources :drivers do
-    resources :trips, except: [:destroy]
-  end
+  resources :drivers
   
   resources :passengers do
     resources :trips, only: %i[create new index]
