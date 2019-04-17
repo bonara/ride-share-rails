bdcfae0b5e0e499d325a07aa589fa601809992ac
Nara <naraa_520@yahoo.com>
2019-04-17 12:12:48 -0700

    Removes shared folder for errors

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root 'homepages#index'
   resources :drivers
   resources :passengers do
-    resources :trips, except: [:destroy]
+    resources :trips, except: %i[create new]
   end
   resources :trips, only: [:destroy]
 end
