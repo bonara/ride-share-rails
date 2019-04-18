24200031d3cf4840ef6c7b70b56157d9c538554c
Carla Bosco <carlabosco@gmail.com>
2019-04-17 18:41:40 -0700

    Adding driver avrg_rating

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -3,4 +3,14 @@ class Driver < ApplicationRecord
 
   validates :name, presence: true
   validates :vin, presence: true, length: { is: 14}
+
+  
+  @driver = Driver.find_by(id: @driver_id)
+  @driver_trips = Trip.where(driver_id: @driver_id)
+  
+  
+  def avrg_rating
+    return total = (@driver_trips.sum {|trip| trip.rating})/@driver_trips.count
+  end
 end
+
