6a83fd814730d31299bfd15cdb5a0648c594c3e3
Carla Bosco <carlabosco@gmail.com>
2019-04-18 15:11:37 -0700

    Updated driver and passenger models with destroy functionality

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -1,5 +1,5 @@
 class Driver < ApplicationRecord
-  has_many :trips
+  has_many :trips, :dependent => :destroy
 
   validates :name, presence: true
   validates :vin, presence: true, length: { is: 14}
@@ -8,12 +8,14 @@ class Driver < ApplicationRecord
 #The driver gets 80% of the trip cost after a fee of $1.65 is subtracted
 
   def total_earnings
-    total = (self.trips.sum {|trip| trip.cost - 165 }) * 0.8
-    return "$#{(total /= 100).round(2)}"
+    unless self.trips == nil
+      total = (self.trips.sum {|trip| trip.cost - 165 }) * 0.8
+      return "$#{(total /= 100).round(2)}"
+    end
   end
   
   def avrg_rating
-    return total = (self.trips.sum { |trip| trip.rating}) / self.trips.count
+    return total = ((self.trips.sum { |trip| trip.rating if trip.rating != nil}) / self.trips.count).floor
   end
 
 end
--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,5 +1,5 @@
 class Passenger < ApplicationRecord
-  has_many :trip
+  has_many :trip, :dependent => :destroy
 
   validates :name, :phone_num, presence: true
 end
